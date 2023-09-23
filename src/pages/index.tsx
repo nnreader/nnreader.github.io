e25cf9dc604fe85df48b4515f93e378f99f113e9
Axetroy <axetroy.dev@gmail.com>
2023-09-24 01:14:24 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,47 @@
 import { Link } from "react-router-dom";
 import { getBooks } from "../lib/assets";
 import styles from "./index.module.less";
-
-const books = await getBooks();
+import { useEffect, useState } from "react";
+import { DotLoading, Toast } from "antd-mobile";
 
 const Index = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState<string[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getBooks()
+      .then((data) => {
+        setBooks(data);
+      })
+      .catch((err) => {
+        Toast.show({
+          icon: "fail",
+          content: err.message,
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
-      <ul>
-        {books.map((book) => {
-          return (
-            <li key={book}>
-              <Link style={{ color: "#fff" }} to={"/book?name=" + btoa(encodeURIComponent(book)) }>
-                {book}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      {isLoading ? (
+        <DotLoading className={styles.loading} />
+      ) : (
+        <ul>
+          {books.map((book) => {
+            return (
+              <li key={book}>
+                <Link style={{ color: "#fff" }} to={"/book?name=" + btoa(encodeURIComponent(book))}>
+                  {book}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
